edb73098a15168aff2a5d704e95e140d8679afaf
samuel cordova <cordovas1337@gmail.com>
2022-07-24 03:34:22 -0500

    testing 5

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,8 @@
 import './style.css';
-import * as THREE from 'three';
+import * as THREE from '../node_modules/three';
 // import * as THREE from 'https://unpkg.com/three@0.127.0/nuild/three.module.js'
 import { TorusGeometry } from './three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { OrbitControls } from '../node_modules/three/examples/jsm/controls/OrbitControls';
 
 // scene == container
 const scene = new THREE.Scene();
